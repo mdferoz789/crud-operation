b7ff186fb6ec8926518e27f951acda0797e73a1f
mdferoz789 <94826061+mdferoz789@users.noreply.github.com>
2022-09-19 23:57:15 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import ApiCall from "./practice/ApiCall";
 import Create from "./components/create/Create";
+import ApiCall2 from "./practice/ApiCAll2";
 
 function App() {
   return (
     <div className="main">
-      <h3>React CRUD Operations</h3>
-      <h4> using react</h4>
-      <Create />
+      {/* <h3>React CRUD Operations</h3> */}
+      {/* <h4> using react</h4> */}
+      {/* <Create /> */}
+      <ApiCall2/>
+      {/* <ApiCall/> */}
     </div>
   );
 }
